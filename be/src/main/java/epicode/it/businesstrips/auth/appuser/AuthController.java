f9c6a7e1111b46e2728d12b7d2b5d8c51b01fedf
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-01-15 09:07:37 +0100

    update register endpoint

--- be/src/main/java/epicode/it/businesstrips/auth/appuser/AuthController.java
+++ be/src/main/java/epicode/it/businesstrips/auth/appuser/AuthController.java
@@ -4,12 +4,14 @@ import epicode.it.businesstrips.auth.dto.AuthResponse;
 import epicode.it.businesstrips.auth.dto.LoginRequest;
 import epicode.it.businesstrips.auth.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -19,12 +21,12 @@ public class AuthController {
     private final AppUserSvc appUserSvc;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<Map<String, String>> register(@RequestBody RegisterRequest registerRequest) {
         appUserSvc.registerUser(
                 registerRequest,
                 Set.of(Role.ROLE_USER) // Assegna il ruolo di default
         );
-        return ResponseEntity.ok("Registrazione avvenuta con successo");
+        return ResponseEntity.ok().body(Map.of("message", "Registration successful"));
     }
 
     @PostMapping("/login")
